2eda28d5686d8ce95e556acb1977caa2c53b74af
Chumarenko Kirill <code0st@gmail.com>
2024-09-25 00:13:51 +0300

    [UPDATE] Добавлен прокси для селективного выполнения прогноза необходимым методом.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import json
+import enum
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import json
-from model import create_model, train_model, inference_model
-import pandas as pd
-from csv_utils import read_file, plot_data, get_column_values
 
-# methods
+from csv_utils import read_file, plot_data, get_column_values
 from methods import ema, sma, holtwinters, lr, neuralnetwork
+
+@enum.unique
+class EPredictMethod(enum.Enum):
+    sma = 'SMA'
+    ema = 'EMA'
+    holt_winters = 'HoltWinters'
+    linear_regression = 'LinearRegression'
+    neural_network = 'NeuralNetwork'
+    arima = 'ARIMA'
 
 app = Flask(__name__)
 
@@ -23,40 +31,48 @@ def file_post_request():
 
 @app.route('/train', methods=['POST'])
 def train_post_request():
-    data = json.loads(request.data)
-    df = pd.read_csv(data['name'], sep=';')
-    print(df.head())
-    model = create_model()
-    trained_model = train_model(df, model, int(data['steps']), int(data['epochs']))
-    inferenced_model = inference_model(df, trained_model, int(data['steps']), int(data['count']))
-    return jsonify({'status': 'OK', 'dates': str(inferenced_model[0]), "values": str(inferenced_model[1]), "metrics": inferenced_model[2]})
+    # TODO: Подумать над общими данными в запросе 
+    # Например, кол-во шагов для прогноза и тд
+    # Также добавить больше конфигурационных параметров для отдельных методов
+    payload = json.loads(request.data)
+    close_values = get_column_values(read_file(payload['name']), column='<CLOSE>')
+    dates_values = get_column_values(read_file(payload['name']), column='<DATE>')
+    
+    # TODO: Нужно унифицировать ответ
+    # test_result = proxy_method(close_prices, payload)
 
+    return jsonify({'status': 'OK', 'close_values': close_values.tolist(), 'dates_values': dates_values.tolist()})
 
-def main():
-    file_path = 'GC.csv'
-    data = read_file(file_path)
-    # plot_data(data, column='<CLOSE>')
-    close_prices = get_column_values(data, column='<CLOSE>')
+def proxy_method(time_series, payload):
+    match payload['method']:
+        case EPredictMethod.sma.value:
+            return sma.test_sma(time_series, payload['window_size'])
+        case EPredictMethod.ema.value:
+            return ema.test_ema(time_series, payload['alpha'])
+        case EPredictMethod.holt_winters.value:
+            return holtwinters.test_holtwinters(time_series, payload['seasonal_periods'])
+        case EPredictMethod.linear_regression.value:
+            return lr.test_lr(time_series, payload['window_size'], payload['steps'])
+        case EPredictMethod.neural_network.value:
+            return neuralnetwork.test_neural_network(time_series, payload['window_size'])
+        case EPredictMethod.arima.value:
+            return None
 
-    predict_neuralnetwork(close_prices)
+# def predict_sma(time_series):
+#     sma.test_sma(time_series, 6)
 
+# def predict_ema(time_series):
+#     ema.test_ema(time_series, 0.7)
 
-def predict_sma(time_series):
-    sma.test_sma(time_series, 6)
+# def predict_holrwinters(time_series): 
+#     holtwinters.test_holtwinters(time_series, 30)
 
-def predict_ema(time_series):
-    ema.test_ema(time_series, 0.7)
+# def predict_lr(time_series):
+#     lr.test_lr(time_series, 15, 5)
 
-def predict_holrwinters(time_series): 
-    holtwinters.test_holtwinters(time_series, 30)
-
-def predict_lr(time_series):
-    lr.test_lr(time_series, 15, 5)
-
-def predict_neuralnetwork(time_series):
-    neuralnetwork.test_neural_network(time_series, 15)
+# def predict_neuralnetwork(time_series):
+#     neuralnetwork.test_neural_network(time_series, 15)
     
 
 if __name__ == "__main__":
-    main()
-    # app.run(host='localhost', port=8080)
+    app.run(host='localhost', port=8080)
--- methods/holtwinters.py
+++ methods/holtwinters.py
@@ -9,15 +9,14 @@ def exponential_smoothing(series, seasonal_periods):
 
     return fit
 
-def exponential_smoothing_forecast(series, steps):
-    return exponential_smoothing(series, steps).forecast(steps)
+def exponential_smoothing_forecast(series, seasonal_periods):
+    return exponential_smoothing(series, seasonal_periods).forecast(seasonal_periods)
 
-def plot_series_with_smoothing(time_series, forecast, steps):    
+def plot_series_with_smoothing(time_series, forecast, seasonal_periods):    
     # Построение графика
     plt.figure(figsize=(10, 6))
     plt.plot(time_series, label='Оригинальные данные')
-    # TODO: 30 заменить на параметр
-    plt.plot(range(len(time_series), len(time_series) + steps), forecast, label='Прогноз', color='orange')
+    plt.plot(range(len(time_series), len(time_series) + seasonal_periods), forecast, label='Прогноз', color='orange')
     plt.title('Прогнозирование методом Хольта-Винтерса')
     plt.xlabel('Время')
     plt.ylabel('Значение')
@@ -25,31 +24,6 @@ def plot_series_with_smoothing(time_series, forecast, steps):
     plt.grid(True)
     plt.show()
 
-def test_holtwinters(time_series, steps):
-    forecast = exponential_smoothing_forecast(time_series, steps)
-    plot_series_with_smoothing(time_series, forecast, steps)
-
-# # Пример временного ряда с сезонностью
-# time_series = [112, 118, 132, 129, 121, 135, 148, 148, 136, 119, 104, 118,
-#                115, 126, 141, 135, 125, 149, 170, 170, 158, 133, 114, 140,
-#                145, 150, 178, 163, 172, 178, 199, 199, 184, 162, 146, 166,
-#                171, 180, 193, 181, 183, 218, 230, 242, 209, 191, 172, 194,
-#                196, 196, 236, 235, 229, 243, 264, 272, 237, 211, 180, 201]
-
-# # Применение метода Хольта-Винтерса с аддитивной сезонностью
-# model = ExponentialSmoothing(time_series, seasonal='add', trend='add', seasonal_periods=12)
-# fit = model.fit()
-
-# # Прогнозирование на 12 периодов вперед
-# forecast = fit.forecast(steps=12)
-
-# # Построение графика
-# plt.figure(figsize=(10, 6))
-# plt.plot(time_series, label='Оригинальные данные')
-# plt.plot(range(len(time_series), len(time_series) + 12), forecast, label='Прогноз', color='orange')
-# plt.title('Прогнозирование методом Хольта-Винтерса')
-# plt.xlabel('Время')
-# plt.ylabel('Значение')
-# plt.legend()
-# plt.grid(True)
-# plt.show()
+def test_holtwinters(time_series, seasonal_periods):
+    forecast = exponential_smoothing_forecast(time_series, seasonal_periods)
+    plot_series_with_smoothing(time_series, forecast, seasonal_periods)
